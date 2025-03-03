a70a05ca6c4e470674249999875f513478f792a3
Kaan Taşcı <k_mert@windowslive.com>
2025-03-04 01:07:54 +0300

    temel değişkenleri ve var dan bahsettim

---
+++ src/main/java/Varmain.java
@@ -0,0 +1,15 @@
+public class Varmain  {
+    public static void main(String[] args) {
+        int var = 123;
+        //String void = "Patika"; void ile değişken tanımı yapılamaz
+        var name = "kaan";
+        var age = 18;
+        // var value ; geçersiz kullanım çünkü ilk değer ataması yok.
+        var value = 10 ;
+        //value = "Patika"; Compile time hatası çünkü bir üst satırda int olduğunu belirtmiştik.
+
+        //veri tipleri uyuşmazlığını gidermek için oluşturulmuş bir tiptir.
+        //kodların sadeleştirmesine genelde yarıyor.Yinede dikkatli kullanılması gerekn bir tiptir.
+
+    }
+}
---
+++ src/main/java/org/example/temelkavramlar.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class temelkavramlar {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+    //yan yana yazırmaz println şeklinde her zaman alt alta yazılır.
+
+        //değişken tanımlara bakalım şimdi :
+        int age = 24 ; //tam sayı veri tipidir.
+        double x = 2.1; // kesirli sayılar kullanılır.
+        byte y = 127; //128 ile -127 arasındadır.
+        char harf1 = 'c'; // 16 bit uzunluğundadır.Karakterlerin karşılığını tutar.
+
+
+        boolean isA; //değeri doğru ya da yanlış olabilecek bir değişken türüdür
+        isA = true;
+        isA = false;
+
+        long l = 36721673; //en büyük sayı veri tipidir.karmaşık işlerde kullanabiliriz.
+         //şu anlık bu kadar hatırladıklarım ve yazdıklarım daha tabikide var ama zaman buldukça ekleyeceğim.
+
+
+    }
+}
